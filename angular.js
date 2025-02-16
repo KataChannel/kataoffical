c8881c15866af4a41c7e5e8c5607623cc47f77c2
quocbao1983 <quocbao280783@gmail.com>
2025-02-16 13:09:17 +0700

    update

--- angular.js
+++ angular.js
@@ -1,8 +1,83 @@
-import { capitalize } from "@angular-devkit/core/src/utils/strings";
+import inquirer from 'inquirer';
 import fs from 'fs-extra';
 import path from 'path';
 import chalk from 'chalk';
-// Generate file with dynamic content
+async function promptUser(type, name, outputDir) {
+  const questions = [];
+  if (!type) {
+    questions.push({
+      type: 'input',
+      name: 'type',
+      message: 'Chọn Loại Ứng Dụng',
+      default: 'nestjs',
+      validate: (input) => (input ? true : 'Type cannot be empty.'),
+    });
+  }
+
+  if (!name) {
+    questions.push({
+      type: 'input',
+      name: 'name',
+      message: 'Tên Ứng Dụng:',
+      validate: (input) => (input ? true : 'Name cannot be empty.'),
+    });
+  }
+
+  if (!outputDir) {
+    questions.push({
+      type: 'input',
+      name: 'outputDir',
+      message: 'Chọn Thư Mục (default: ./src):',
+      default: './src',
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+
+  return {
+    type: type || answers.type,
+    name: name || answers.name,
+    outputDir: outputDir || answers.outputDir,
+  };
+}
+
+// Parse command-line arguments
+function parseArgs() {
+  const args = process.argv.slice(2);
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace('--', '');
+    const value = args[i + 1];
+    parsedArgs[key] = value;
+  }
+
+  return parsedArgs;
+}
+
+// Main function
+(async function main() {
+  const args = parseArgs();
+
+  const answers = await promptUser(args.type, args.name, args.outputDir);
+  console.log(answers);
+
+  switch (answers.type) {
+    case 'nestjs':
+      console.log('nestjs');
+      await generateNestFiles(answers);
+      break;
+    case 'angular':
+      console.log('angular');
+      await generateAngularFiles(answers);
+      break;
+    default:
+      console.log('Invalid type');
+      break;
+  }
+})();
+
+
 async function generateFile(filePath, content) {
     try {
       await fs.ensureDir(path.dirname(filePath));
--- index.js
+++ index.js
@@ -2,32 +2,83 @@
 import inquirer from 'inquirer';
 import { generateNestFiles } from './nestjs.js';
 import { generateAngularFiles } from './angular.js';
-// Prompt user for details
-async function promptUser() {
-  const questions = [
-    {
+import inquirer from 'inquirer';
+
+async function promptUser(type, name, outputDir) {
+  const questions = [];
+
+  if (!type) {
+    questions.push({
       type: 'input',
       name: 'type',
       message: 'Chọn Loại Ứng Dụng',
       default: 'nestjs',
-      validate: (input) => (input ? true : 'Name cannot be empty.'),
-    },
-    {
+      validate: (input) => (input ? true : 'Type cannot be empty.'),
+    });
+  }
+
+  if (!name) {
+    questions.push({
       type: 'input',
       name: 'name',
-      message: 'Tên ứng Dụng:',
+      message: 'Tên Ứng Dụng:',
       validate: (input) => (input ? true : 'Name cannot be empty.'),
-    },
-    {
+    });
+  }
+
+  if (!outputDir) {
+    questions.push({
       type: 'input',
       name: 'outputDir',
       message: 'Chọn Thư Mục (default: ./src):',
       default: './src',
-    },
-  ];
+    });
+  }
 
-  return inquirer.prompt(questions);
+  const answers = await inquirer.prompt(questions);
+
+  return {
+    type: type || answers.type,
+    name: name || answers.name,
+    outputDir: outputDir || answers.outputDir,
+  };
 }
+
+// Parse command-line arguments
+function parseArgs() {
+  const args = process.argv.slice(2);
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace('--', '');
+    const value = args[i + 1];
+    parsedArgs[key] = value;
+  }
+
+  return parsedArgs;
+}
+
+// Main function
+(async function main() {
+  const args = parseArgs();
+
+  const answers = await promptUser(args.type, args.name, args.outputDir);
+  console.log(answers);
+
+  switch (answers.type) {
+    case 'nestjs':
+      console.log('nestjs');
+      await generateNestFiles(answers);
+      break;
+    case 'angular':
+      console.log('angular');
+      await generateAngularFiles(answers);
+      break;
+    default:
+      console.log('Invalid type');
+      break;
+  }
+})();
 
 // // Generate file with dynamic content
 // async function generateFile(filePath, content) {
@@ -269,21 +320,4 @@ async function promptUser() {
 //   await generateFile(entityFile, entityContent);
 // }
 
-// Main function
-(async function main() {
-  const answers = await promptUser();
-  console.log(answers);
-  switch (answers.type) {
-    case 'nestjs':
-      console.log('nestjs');
-      await generateNestFiles(answers);
-      break;
-    case 'angular':
-      console.log('angular');
-      await generateAngularFiles(answers);
-      break;
-    default:
-      console.log('Invalid type');
-      break;
-  }
-})();
+
