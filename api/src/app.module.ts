56fc00eb6c7c188cb9398484d90ccdeae1effeca
quocbao1983 <quocbao280783@gmail.com>
2025-02-19 21:28:58 +0700

    update

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma.module';
-import { PrismaService } from './prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [AuthModule, UserModule,PrismaModule],
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(dto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: dto.password, // Hash password in real app
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getUsers() {
+    return this.prisma.user.findMany();
   }
 }
