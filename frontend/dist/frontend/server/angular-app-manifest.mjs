2459f9e9ad0cd88ad8f4f2f824b5bff289707a75
chikiet88 <chikiet88@gmail.com>
2025-03-18 11:16:44 +0700

    update

--- frontend/dist/frontend/server/angular-app-manifest.mjs
+++ frontend/dist/frontend/server/angular-app-manifest.mjs
@@ -873,8 +873,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 72816, hash: '75806b3c3bad4837d7d6100ecfa01fbc1c162235e98c303fedcaabb00a8fe193', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 24107, hash: '62b7cb6dfba474e1d8cd2c90259b64e1b9f30902575c023b223622dec50c39bc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-QKTZODPE.css': {size: 370263, hash: '7vvBtnugnVM', text: () => import('./assets-chunks/styles-QKTZODPE_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 72816, hash: '71649a78ccb9d2bdbc708f33c39b4dfe071c06cd64c407d74f7ab58521f53256', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 24107, hash: '081cae5d8c727f488ca52e97b8aa80eb5e288821dda2e823fe7195d7333573b9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-CPQPAPJ3.css': {size: 370378, hash: 'a3jXyPVbpgk', text: () => import('./assets-chunks/styles-CPQPAPJ3_css.mjs').then(m => m.default)}
   },
 };
