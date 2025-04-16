304896760f738fee32838f9f9b5e69ae1e666748
chikiet88 <chikiet88@gmail.com>
2025-04-16 09:51:35 +0700

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/kataoffical',
+    publicPath: '/kataoffical/dist',
   },
   plugins: [
     // Sao chép thư mục assets vào thư mục dist
