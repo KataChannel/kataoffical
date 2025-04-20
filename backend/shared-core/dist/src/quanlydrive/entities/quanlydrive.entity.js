4edfc7109d9ebec7caf72570e57b74feaf7f429b
quocbao1983 <quocbao280783@gmail.com>
2025-04-20 22:47:11 +0700

    update

--- backend/shared-core/dist/src/quanlydrive/entities/quanlydrive.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Quanlydrive = void 0;
-class Quanlydrive {
-}
-exports.Quanlydrive = Quanlydrive;
-//# sourceMappingURL=quanlydrive.entity.js.map
