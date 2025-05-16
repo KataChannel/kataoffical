0aa555e5e6784c1b76b8b6e048e23679e4664cc6
chikiet88 <chikiet88@gmail.com>
2025-05-16 14:29:41 +0700

    update

--- site/dist/site/server/angular-app-manifest.mjs
+++ site/dist/site/server/angular-app-manifest.mjs
@@ -1054,8 +1054,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 72766, hash: '3632b0e9773eced778638e23e04428189a4725e78dab20cfaf85b386dbcc7099', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 24057, hash: '6f7a9034947813ad12af71d3d668b9fd09d0c71717180c49a614cfc3edfc5abf', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 72766, hash: '27af5c0217a8cd96c89b007d855c195693ba1abc74b8a04a25f0897aa4c45b77', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 24057, hash: 'a08625481f9b42d329b1786a0f263858de0076bc35061da4c87b8df6551685c8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-AURRFZS6.css': {size: 144903, hash: 'qlHw6XxGqg4', text: () => import('./assets-chunks/styles-AURRFZS6_css.mjs').then(m => m.default)}
   },
 };
