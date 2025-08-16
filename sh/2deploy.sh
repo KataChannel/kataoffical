a1b70c19d3edecc7fd9c4d3c604e9fef788ed995
Kata Channel <katachanneloffical@gmail.com>
2025-08-16 15:51:30 +0700

    update

--- sh/2deploy.sh
+++ sh/2deploy.sh
@@ -9,7 +9,7 @@ git push
 ssh root@116.118.49.243 << 'EOF'
 cd rausach
 git pull
-docker compose -f 'docker-compose.yml' build --no-cache berausach
-docker compose -f 'docker-compose.yml' up -d
+docker compose -f 'docker-compose.yml' up -d --build
 docker builder prune -af
+#docker image prune -a -f
 EOF
