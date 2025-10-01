f50ea55b6f507e8b27af46413175488b7db27211
Kata Channel <katachanneloffical@gmail.com>
2025-10-01 10:02:04 +0700

    Update

--- frontend/dist/frontend/browser/chunk-PJIGDCMK.js
+++
@@ -1,1 +0,0 @@
-import{Db as n,Dc as r,ec as o,fc as s}from"./chunk-OL5DVADB.js";import"./chunk-LWLSKGAV.js";import"./chunk-6DYNGEAS.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-register"]],decls:2,vars:0,template:function(e,a){e&1&&(o(0,"p"),r(1,"register works!"),s())},encapsulation:2})};export{i as RegisterComponent};
