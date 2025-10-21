48c6001d279e5619d66f608f2a45beb167c962c0
Chí Kiệt <chikiet88@gmail.com>
2025-10-21 08:31:52 +0700

    Update

--- api/dist/prisma/updatesldatdonhang.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function convertData(data1: any[]): {
-    banggiaId: string;
-    khachhangIds: any[];
-}[];
-export declare function removeVietnameseAccents(text: any): any;
-export declare function DonhangcodeToNumber(code: any): number;
-export declare function DonhangnumberToCode(number: any): string;
--- api/dist/src/nhacungcap/entities/nhacungcap.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Nhacungcap = void 0;
-class Nhacungcap {
-}
-exports.Nhacungcap = Nhacungcap;
-//# sourceMappingURL=nhacungcap.entity.js.map
