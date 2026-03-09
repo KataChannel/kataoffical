d865275322e8a62d30ee0ad7ad3f6fc06cf1b7b8
Chí Kiệt <chikiet88@gmail.com>
2026-03-09 15:54:32 +0700

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,8 @@ git push
 # Remote server operations
 ssh root@116.118.49.243 << 'EOF'
 cd rausachfinal
+git checkout .
 git pull
 docker compose -f 'docker-compose.yml' up -d --build
 docker builder prune -af
-#docker image prune -a -f
 EOF
